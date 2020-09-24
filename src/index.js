49a99a7eee4072a0c63c30878b3d3ebf32bfac06
susu-git92 <chlojeong@gamil.com>
2020-09-24 21:27:20 +0900

    added calculating function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState}from 'react';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const [data, setData] = useState("");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const calcBtns = [];
+  [9,8,7,6,5,4,3,2,1,0,".","%"].forEach(num => {
+    calcBtns.push (
+      <button onClick = {e => {
+        setData(data + e.target.value)
+      }}
+      value={num}
+      key={num} 
+      >
+        {num}
+      </button>  
+    )
+  })
+  return (
+    <div className="wrapper">
+      <div className= "result">{data}</div> 
+      <div className = "digits-flex">{calcBtns}</div>
+      <div className = "modifiers subgrid">
+        <button onClick={() => setData(data.substr(0, data.length -1))}>
+          Clear
+        </button>
+        <button onClick={()=> setData("")}>
+          AC
+        </button>
+      </div>
+
+      <div className="operations subgrid">
+        <button onClick={e => setData(data + e.target.value)} value="+">
+          +
+        </button>
+        <button onClick={e => setData(data + e.target.value)} value="-">
+          -
+        </button>
+        <button onClick={e => setData(data + e.target.value)} value="*">
+          *
+        </button>
+        <button onClick={e => setData(data + e.target.value)} value="/">
+          /
+        </button>
+        <button onClick={e => {
+          try
+        {
+          setData(
+            String(eval(data)).length > 3 &&
+            String(eval(data)).includes("") 
+            ? String(eval(data).toFixed(4))
+            : String(eval(data))
+          )
+        }
+          catch(err) {
+            console.log(err);
+          } 
+        }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
